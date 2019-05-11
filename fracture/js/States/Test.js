fee3ca00e16dcb8c627c716f7e029f7feebb428a
Sanchit Keni <sskeni@ucsc.edu>
2019-05-10 20:38:11 -0700

    added medium and large platforms

--- fracture/js/States/Test.js
+++ fracture/js/States/Test.js
@@ -6,6 +6,8 @@ var Test = {
 
         //tileset assets
         game.load.image('smallplatform', 'smallplatform.png');
+        game.load.image('mediumplatform', 'mediumplatform.png');
+        game.load.image('largeplatform', 'largeplatform.png');
         game.load.tilemap('test', 'test.json', null, Phaser.Tilemap.TILED_JSON);
         game.load.spritesheet('tilesheet', 'testtileset.png');
     },
@@ -26,17 +28,33 @@ var Test = {
         this.mapLayer = this.map.createLayer('walls');
         this.mapLayer.resizeWorld();
 
-        //add platforms
-        this.platforms = game.add.group();
-        this.platforms.enableBody = true;
-        this.platforms.physicsBodyType = Phaser.Physics.P2JS;
-        this.map.createFromObjects('smallplatforms', 5, 'smallplatform', 0, true, false, this.platforms);
+        //add small platforms
+        this.smallplatforms = game.add.group();
+        this.smallplatforms.enableBody = true;
+        this.smallplatforms.physicsBodyType = Phaser.Physics.P2JS;
+        this.map.createFromObjects('smallplatforms', 5, 'smallplatform', 0, true, false, this.smallplatforms);
+
+        //add medium platforms
+        this.mediumplatforms = game.add.group();
+        this.mediumplatforms.enableBody = true;
+        this.mediumplatforms.physicsBodyType = Phaser.Physics.P2JS;
+        this.map.createFromObjects('mediumplatforms', 6, 'mediumplatform', 0, true, false, this.mediumplatforms);
+
+        //add large platforms
+        this.largeplatforms = game.add.group();
+        this.largeplatforms.enableBody = true;
+        this.largeplatforms.physicsBodyType = Phaser.Physics.P2JS;
+        this.map.createFromObjects('largeplatforms', 7, 'largeplatform', 0, true, false, this.largeplatforms);
 
         //move platforms to account for anchor
-        this.platforms.forEach(this.changeBody, this);
+        this.smallplatforms.forEach(this.changeBody, this);
+        this.mediumplatforms.forEach(this.changeBody, this);
+        this.largeplatforms.forEach(this.changeBody, this);
 
         //set platforms to kinematic
-        this.platforms.setAll('body.kinematic', true);
+        this.smallplatforms.setAll('body.kinematic', true);
+        this.mediumplatforms.setAll('body.kinematic', true);
+        this.largeplatforms.setAll('body.kinematic', true);
 
         //add p2 physics to tilemap
         game.physics.p2.convertTilemap(this.map, this.mapLayer);
@@ -44,7 +62,9 @@ var Test = {
         //set colors for map
         color = Math.random() * 0xffffff;
         this.mapLayer.tint = color;
-        this.platforms.setAll('tint', color);
+        this.smallplatforms.setAll('tint', color);
+        this.mediumplatforms.setAll('tint', color);
+        this.largeplatforms.setAll('tint', color);
 
         //create player
         this.player = new Player(game, 100, 200, 'caretaker');
